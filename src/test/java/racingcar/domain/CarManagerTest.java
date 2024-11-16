3fcc4a1949319e6b89df20c154a4b2a87815ad8b
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-16 16:56:05 +0900

    test: 자동차 move 테스트
    랜덤 번호에 따른 케이스에 대해 자동차가 움직이는 테스트를 작성하였음

---
+++ src/test/java/racingcar/domain/CarManagerTest.java
@@ -0,0 +1,81 @@
+package racingcar.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarManagerTest {
+
+    private CarManager carManager;
+
+    @BeforeEach
+    void carsInit() {
+        List<Car> cars = List.of(
+                Car.from("pobi"),
+                Car.from("jenny"),
+                Car.from("jisu")
+        );
+        carManager = CarManager.from(cars);
+    }
+
+    @DisplayName("5보다 작은 랜덤번호가 주어졌을 때, 모든 자동차는 움직여서는 안 된다.")
+    @Test
+    void under_move_condition_then_not_move() {
+
+        //given
+        List<Integer> randomNumbers = List.of(1, 2, 3);
+
+        //when
+        carManager.moveCars(randomNumbers);
+        List<Car> cars = carManager.getCars();
+
+        //then
+        for (Car car : cars) {
+            assertThat(car.getDistance()).isEqualTo(0);
+        }
+    }
+
+    @DisplayName("5보다 큰 랜덤번호가 주어졌을 때, 모든 자동차들은 움직여야한다.")
+    @Test
+    void over_move_condition_then__move() {
+
+        //given
+        List<Integer> randomNumbers = List.of(5, 6, 7);
+
+        //when
+        carManager.moveCars(randomNumbers);
+        List<Car> cars = carManager.getCars();
+
+        //then
+        for (Car car : cars) {
+            assertThat(car.getDistance()).isEqualTo(1);
+        }
+    }
+
+    @DisplayName("3, 4, 5 가 주어졌을 경우, 5가 주어진 자동차만 움직여야한다.")
+    @Test
+    void different_case() {
+
+        //given
+        List<Integer> randomNumbers = List.of(3, 4, 5);
+
+        //when
+        carManager.moveCars(randomNumbers);
+        List<Car> cars = carManager.getCars();
+
+        //then
+        for (int i = 0; i < cars.size(); i++) {
+            int randomNumber = randomNumbers.get(i);
+            Car car = cars.get(i);
+
+            if (randomNumber < 5) {
+                assertThat(car.getDistance()).isEqualTo(0);
+                return;
+            }
+            assertThat(car.getDistance()).isEqualTo(1);
+        }
+    }
+}
