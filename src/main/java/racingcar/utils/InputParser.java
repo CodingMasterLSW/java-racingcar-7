7d33c34cd269574381d01a27085aa16902e95669
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-16 15:07:54 +0900

    feat: 입력값 파싱
    입력받은 자동차를 ',' 기준으로 나눈 후, List로 반환한다

---
+++ src/main/java/racingcar/utils/InputParser.java
@@ -0,0 +1,19 @@
+package racingcar.utils;
+
+import java.util.List;
+
+public class InputParser {
+
+    private static final String SPLITTER = ",";
+
+    private InputParser() {
+    }
+
+    public static InputParser create() {
+        return new InputParser();
+    }
+
+    public List split(String userInput) {
+        return List.of(userInput.split(SPLITTER));
+    }
+}
