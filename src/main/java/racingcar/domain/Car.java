0a2938bd052663980644e6792bf4dfaebbf7e3ba
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-16 16:35:13 +0900

    feat: 단일 자동차 move 메서드 구현
    random 번호가 MOVE_CONDITION을 충족할 경우, distance를 증가시킨다.

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -5,6 +5,7 @@ import static racingcar.exception.ErrorMessage.INVALID_CAR_NAME_LENGTH;
 public class Car {
 
     private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final int MOVE_CONDITION = 4;
 
     private final String name;
     private int distance;
@@ -19,7 +20,13 @@ public class Car {
         return new Car(name);
     }
 
-    public void validateNameLength(String name) {
+    public void move(int randomNumber) {
+        if (randomNumber >= MOVE_CONDITION) {
+            this.distance ++;
+        }
+    }
+
+    private void validateNameLength(String name) {
         if (name.length() > MAXIMUM_NAME_LENGTH) {
             throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH.getMessage());
         }
