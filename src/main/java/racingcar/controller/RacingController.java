288c7e0f39bad80c87d33cd87bfc2b851e959b43
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-16 17:53:47 +0900

    refactor: controller 메서드 분리 및 retry 도입

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,6 +1,7 @@
 package racingcar.controller;
 
 import java.util.List;
+import java.util.function.Supplier;
 import racingcar.domain.Car;
 import racingcar.service.RacingService;
 import racingcar.view.InputView;
@@ -12,7 +13,6 @@ public class RacingController {
     private final OutputView outputView;
     private final RacingService racingService;
 
-
     public RacingController(InputView inputView, OutputView outputView, RacingService racingService) {
         this.inputView = inputView;
         this.outputView = outputView;
@@ -20,25 +20,49 @@ public class RacingController {
     }
 
     public void start() {
-        inputView.printInputMessage();
-        String userInput = inputView.racingCarNameInput();
-
-        inputView.printTryNumberMessage();
-        int tryNumber = inputView.tryNumberInput();
+        String userInput = manageCarInput();
+        int tryNumber = manageTryNumberInput();
 
         racingService.initCarManager(userInput);
+        playRacing(tryNumber);
+        winner();
+    }
 
+    private String manageCarInput() {
+        inputView.printInputMessage();
+        String userInput = retryOnInvalidInput(() -> inputView.racingCarNameInput());
+        return userInput;
+    }
+
+    private int manageTryNumberInput() {
+        inputView.printTryNumberMessage();
+        int tryNumber = retryOnInvalidInput(() -> inputView.tryNumberInput());
+        return tryNumber;
+    }
+
+    private void playRacing(int tryNumber) {
         outputView.printRunResultMessage();
-
         for (int i=0; i<tryNumber; i++) {
             List<Integer> numbers = racingService.generateRandomNumbers();
             List<Car> racingResult = racingService.racing(numbers);
             outputView.printRacingResult(racingResult);
         }
+    }
 
+    private void winner(){
         List<String> winner = racingService.winner();
         outputView.printWinnerMessage();
         outputView.printWinnerResult(winner);
+    }
+
+    private <T> T retryOnInvalidInput(Supplier<T> input) {
+        while (true) {
+            try {
+                return input.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printErrorMessage(e.getMessage());
+            }
+        }
     }
 
 }
