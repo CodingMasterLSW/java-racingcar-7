2f474cce351dbc4d8edee0bc776b21153395e7ad
CodingMasterLSW <108395670+CodingMasterLSW@users.noreply.github.com>
2024-11-16 15:24:05 +0900

    feat: 자동차 생성 구현
    자동차 이름이 5를 넘어선 경우, 예외를 발생시킨다

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,35 @@
+package racingcar.domain;
+
+import static racingcar.exception.ErrorMessage.INVALID_CAR_NAME_LENGTH;
+
+public class Car {
+
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+
+    private final String name;
+    private int distance;
+
+    private Car(String name) {
+        validateNameLength(name);
+        this.name = name;
+        this.distance = 0;
+    }
+
+    public static Car from(String name) {
+        return new Car(name);
+    }
+
+    public void validateNameLength(String name) {
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException(INVALID_CAR_NAME_LENGTH.getMessage());
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+}
--- src/main/java/racingcar/exception/ErrorMessage.java
+++ src/main/java/racingcar/exception/ErrorMessage.java
@@ -3,7 +3,8 @@ package racingcar.exception;
 public enum ErrorMessage {
 
     INVALID_INPUT("입력을 하지 않았습니다. 다시 입력해주세요."),
-    ONLY_NUMBER("양의 정수만 입력 가능합니다. 다시 입력해주세요.");
+    ONLY_NUMBER("양의 정수만 입력 가능합니다. 다시 입력해주세요."),
+    INVALID_CAR_NAME_LENGTH("자동차의 이름은 최대 5글자 입니다. 다시 입력해주세요.");
 
     private static final String ERROR_PREFIX = "[ERROR] ";
     private final String message;
